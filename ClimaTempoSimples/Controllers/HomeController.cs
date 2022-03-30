bda961c616a67da76832b37c783ec82327c65072
Rodrigo Vieira <rodrigovf21021994@gmail.com>
2022-03-30 00:44:15 -0300

    Melhora robusta na escabilidade do webApi, implementacao da tabela cidades

---
+++ ClimaTempoSimples/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClimaTempoSimples.Models;
+
+namespace ClimaTempoSimples.Controllers
+{
+    public class HomeController : Controller
+    {
+        ClimaTempoDBEntities climaTempoData = new ClimaTempoDBEntities();
+        public ActionResult Index()
+        {
+            return View(climaTempoData);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string CidadeId)
+        {
+            ViewData["CidadeId"] = CidadeId;
+            return View(climaTempoData);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+
+    }
+}
